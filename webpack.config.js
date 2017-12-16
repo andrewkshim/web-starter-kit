618c03880922818cdde3aab8ca6f77c2326f0fd5
Andrew Shim <andrewkshim92@gmail.com>
2017-12-16 13:33:33 -0800

    :wrench: Added webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    contentBase: [
+      path.resolve(__dirname, 'public'),
+    ]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
