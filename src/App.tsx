ee767c88c3fb003b643422695cd6fcea31b74873
Andrew Shim <andrewkshim92@gmail.com>
2018-01-23 16:47:51 -0800

    :sparkles: Hooked up the rest of the app to redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,61 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+
+// @ts-ignore: react-hot-loader@4.0 does not have typings right now
 import { hot } from 'react-hot-loader';
 
 import * as classes from './App.css';
 
+import { IState } from './dux/index';
+import compose from './utils/compose';
+
+import {
+  ActionCreator,
+  decrement,
+  increment,
+} from './dux/counter';
+
 interface IProps {
-  message: string,
+  counter: number;
+  message: string;
+
+  decrement: ActionCreator;
+  increment: ActionCreator;
 }
+
+const enhance = compose(
+  hot(module),
+  connect(
+    (state: IState) => ({
+      counter: state.counter.value,
+    }),
+    {
+      decrement,
+      increment,
+    },
+  ),
+);
 
 class App extends React.Component<IProps, {}> {
   render() {
-    const { message } = this.props;
+    const {
+      counter,
+      message,
+      increment,
+      decrement,
+    } = this.props;
 
     return (
       <h1 className={ classes.root }>
         { message }
+        <div>
+          { counter }
+          <button onClick={() => increment()}>INCREMENT</button>
+          <button onClick={() => decrement()}>DECREMENT</button>
+        </div>
       </h1>
     );
   }
 }
 
-export default hot(module)(App);
+export default enhance(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './App';
+import reducers, { Store } from './dux';
+
+const store: Store = createStore(reducers);
 
 ReactDOM.render(
-  <App message='web-starter-kit' />,
+  <Provider store={store}>
+    <App message='web-starter-kit' />
+  </Provider>,
   document.getElementById('root'),
 );
