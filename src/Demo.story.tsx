7f337607c52df55cdcb3ae89de1ae85ae33d1fee
Andrew Shim <andrewkshim92@gmail.com>
2018-01-28 08:03:56 -0800

    :sparkles: Added Demo story

---
+++ src/Demo.story.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('Demo', module)
+  .add('default', () => (
+    <div>
+      Hello Storybook!
+    </div>
+  ));
